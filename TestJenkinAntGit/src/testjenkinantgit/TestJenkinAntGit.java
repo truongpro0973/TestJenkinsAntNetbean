6208c6e1305238eae1b7b6aba00f1f44361d8061
TruongNguyen <truongpro1920@gmail.com>
2016-11-21 10:19:19 +0700

    Create project

---
+++ TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
@@ -0,0 +1,20 @@
+/*
+ *  By TruongNguyenAx
+ */
+package testjenkinantgit;
+
+/**
+ *
+ * @author TruongNguyen
+ */
+public class TestJenkinAntGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           System.out.println("Hello World - Github");
+    }
+    
+}
