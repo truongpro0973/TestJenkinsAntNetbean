e995f384975093dd1ef5b4e75c7ff46c1de74508
TruongNguyen <truongpro1920@gmail.com>
2016-11-21 14:07:26 +0700

    bug build

--- TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
+++ TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
@@ -15,6 +15,7 @@ public class TestJenkinAntGit {
     public static void main(String[] args) {
         // TODO code application logic here
            System.out.println("Hello World - Github change 6");
+           bug build
            System.out.println("O o");
     }
     
