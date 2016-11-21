8229a2ab2c5c41a6cfe8c1dfccd1efe9f98a1a4b
TruongNguyen <truongpro1920@gmail.com>
2016-11-21 14:09:55 +0700

    bug runtime

--- TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
+++ TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
@@ -12,11 +12,13 @@ public class TestJenkinAntGit {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
            System.out.println("Hello World - Github change 6");
-           bug build
-           System.out.println("O o");
+           
+           System.out.println("bug runtime");
+           
+           throw  new Exception("Throw runtime");
     }
     
 }
