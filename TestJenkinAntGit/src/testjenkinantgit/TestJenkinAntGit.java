7045c61053dce662dcc63eb445dd42c79140a49b
TruongNguyen <truongpro1920@gmail.com>
2016-11-21 14:12:11 +0700

    finisher

--- TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
+++ TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
@@ -11,14 +11,15 @@ public class TestJenkinAntGit {
 
     /**
      * @param args the command line arguments
+     * 
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         // TODO code application logic here
            System.out.println("Hello World - Github change 6");
            
-           System.out.println("bug runtime");
+           System.out.println("finished!!!");
            
-           throw  new Exception("Throw runtime");
+           //throw  new Exception("Throw runtime");
     }
     
 }
