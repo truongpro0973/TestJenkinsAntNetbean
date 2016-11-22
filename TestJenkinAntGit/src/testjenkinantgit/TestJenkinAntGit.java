cc29f385ff357118d2a7e8b2c8274d7f64ad14db
TruongNguyen <truongpro1920@gmail.com>
2016-11-22 23:05:54 +0700

    change 8

--- TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
+++ TestJenkinAntGit/src/testjenkinantgit/TestJenkinAntGit.java
@@ -15,7 +15,7 @@ public class TestJenkinAntGit {
      */
     public static void main(String[] args)  {
         // TODO code application logic here
-           System.out.println("Hello World - Github change 7");
+           System.out.println("Hello World - Github change 8");
            
            System.out.println("finished!!!");
            
